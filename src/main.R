5ca5200bf2dd8b9576c9c20b860578e9ff52ce68
John Ricco <john.ricco@yale.edu>
2023-07-24 21:09:42 -0400

    Add requirements file.

--- src/main.R
+++ src/main.R
@@ -3,3 +3,14 @@
 #-----------------------------------------
 
 
+# Load required packages
+lapply(readLines('requirements.txt'), library, character.only = T)
+
+# Configure settings
+
+
+# Load data
+
+# Run scenarios
+
+# Produce output
