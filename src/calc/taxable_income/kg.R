3ce1c55c0420fab0c95b2c055c8afa7454c11878
John Ricco <john.ricco@yale.edu>
2023-08-10 11:35:43 -0400

    Add skeleton of capital gains function

---
+++ src/calc/taxable_income/kg.R
@@ -0,0 +1,40 @@
+#-------------------------------------------------------
+# Function to calculate capital gains includable in AGI
+#-------------------------------------------------------
+
+
+calc_kg = function(tax_unit) {
+  
+  #----------------------------------------------------------------------------
+  # Calculates capital gains includable in AGI, i.e. implements Schedule D
+  # form logic. 
+  # 
+  # Parameters:
+  #   - tax_unit (df | list) : either a dataframe or list containing required
+  #                            variables (listed below)
+  #
+  # Returns: dataframe of following variables:
+  #          - txbl_kg (dbl) : net capital gain includable in AGI
+  #----------------------------------------------------------------------------
+  
+  req_vars = c(
+    '...',               # TODO
+    'agi.kg_loss_limit', # (int, law) maximum deductible capital loss
+    'agi.kg_excl_rate'   # (dbl, law) share of capital gains excluded from AGI 
+  )
+  
+  # Parse tax unit object passed as argument
+  tax_unit %>% 
+    parse_calc_fn_input(req_vars) %>% 
+    mutate(
+      
+      # TODO form logic
+      
+      
+    ) %>% 
+    
+    # Keep variables to return
+    select(txbl_kg) %>% 
+    return()
+}
+
