f57a633b37d8131a4ec2b3186088a0594c338d5b
John Ricco <john.ricco@yale.edu>
2023-08-17 21:40:22 -0400

    Add AGI calc function

--- src/calc/functions/income/agi.R
+++ src/calc/functions/income/agi.R
@@ -3,7 +3,7 @@
 #---------------------------------------------------------
 
 
-calc_agi = function(tax_unit) {
+calc_agi = function(tax_unit, fill_missings = F) {
   
   #----------------------------------------------------------------------------
   # Calculates Adjusted Gross Income (AGI). 
@@ -11,74 +11,126 @@ calc_agi = function(tax_unit) {
   # Parameters:
   #   - tax_unit (df | list) : either a dataframe or list containing required
   #                            variables (listed below)
+  #   - fill_missings (bool) : whether to populate any unsupplied variables
+  #                            with 0s (used in testing, not in simulation)
   #
   # Returns: dataframe of following variables:
-  #          - TODO
+  #          - txbl_ss    (dbl) Social Security benefits included in AGI
+  #          - sl_int_ded (dbl) student loan interest deduction
+  #          - gross_inc  (dbl) gross income
+  #          - above_ded  (dbl) above-the-line deductions
+  #          - agi        (dbl) Adjusted Gross Income
   #----------------------------------------------------------------------------
   
   req_vars = c(
     
     # Tax unit attributes
     
-    'wages',          # (dbl) W2 wages after pre-tax deductions
-    'txbl_int',       # (dbl) taxable interest income 
-    'exempt_int',     # (dbl) tax-exempt interest income
-    'div',            # (dbl) total dividend income
-    'txbl_ira_dist',  # (dbl) taxable IRA distributions
-    'txbl_pens_dist', # (dbl) taxable DB and DC pension distributions plus
-                      #       annuity payments
-    'gross_ss',       # (dbl) gross OASI benefits
-    'txbl_kg',        # (dbl) net capital gain includable in AGI
-    'state_ref',      # (dbl) taxable refunds/credits/offsets of SALT
-    'alimony',        # (dbl) taxable alimony income
-    'divorce_year',   # (int) year of divorce if applicable
-    'sole_prop',      # (dbl) sole proprietor's net income (Sch. C)
-    'sch_e',          # (dbl) net partnership, S corp, rental, royalty, estate,
-                      #       and trust income (Sch E.)
-    'farm',           # (dbl) net farm income (Sch. F)
-    'ui',             # (dbl) gross unemployment benefits
-    'nols',           # (dbl) net operating losses
-    'other',          # (dbl) all other income sources: gambling, debt 
-                      #       cancellation, etc. See Sch. 1
+    'wages',           # (dbl) W2 wages after pre-tax deductions
+    'txbl_int',        # (dbl) taxable interest income 
+    'exempt_int',      # (dbl) tax-exempt interest income
+    'div',             # (dbl) total dividend income
+    'txbl_ira_dist',   # (dbl) taxable IRA distributions
+    'txbl_pens_dist',  # (dbl) taxable DB and DC pension distributions plus
+                       #       annuity payments
+    'gross_ss',        # (dbl) gross OASI benefits
+    'txbl_kg',         # (dbl) net capital gain includable in AGI
+    'state_ref',       # (dbl) taxable refunds/credits/offsets of SALT
+    'alimony',         # (dbl) alimony income
+    'divorce_year',    # (int) year of divorce if applicable
+    'sole_prop',       # (dbl) sole proprietor's net income (Sch. C)
+    'sch_e',           # (dbl) net partnership, S corp, rental, royalty, estate,
+                       #       and trust income (Sch E.)
+    'farm',            # (dbl) net farm income (Sch. F)
+    'ui',              # (dbl) gross unemployment benefits
+    'nols',            # (dbl) net operating losses
+    'other_inc',       # (dbl) all other income sources: gambling, debt 
+                       #       cancellation, etc. See Sch. 1
+    'ed_exp',          # (dbl) educator expenses
+    'hsa_contr',       # (dbl) pretax contributions to an HSA
+    'liab_seca_er',    # (dbl) "employer"-side SECA liability
+    'trad_contr_ira',  # (dbl) pretax contributions to an IRA
+    'sl_int_exp',      # (dbl) student loan interest paid
+    'keogh_contr',     # (dbl) contributions to SEP plans and KOEGH accounts
+    'se_health',       # (dbl) self-employed health insurance premiums paid
+    'early_penalty',   # (dbl) penalty on early withdrawal from retirement account
+    'alimony_exp',     # (dbl) alimony paid
+    'tuition_ded',     # (dbl) deductible tuition and fees (pre-2021 definition)
+    'dpad',            # (dbl) domestic production activities deduction
+    'other_above_ded', # (dbl) other deductions per Schedule 1 line 24
     
     # Tax law attributes
     
-    'agi.alimony_repeal_year' # (int)
+    'agi.alimony_repeal_year', # (int) year during and after which a divorce does 
+                               #       not generate taxable/deductible alimony
+    'agi.sl_limit',            # (int) maximum deductible student loan interest
+    'agi.sl_po_thresh',        # (int) MAGI phaseout threshold for student loan
+                               #       interest deduction
+    'agi.sl_po_range',         # (int) MAGI phaseout range for student loan
+                               #       interest deduction
+    'agi.tuition_ded_limit',   # (int) limit on tuition and feeds deduction 
+    'agi.dpad_limit'           # (int) limit on domestic production activities
+                               #       deduction
   )
   
   tax_unit %>% 
-   
+    
     # Parse tax unit object passed as argument
-    parse_calc_fn_input(req_vars) %>% 
+    parse_calc_fn_input(req_vars, fill_missings) %>% 
     mutate(
       
-      
-      # Determine whether alimony is includable TODO
-      txbl_alimony = alimony * (divorce_year < agi.alimony_repeal_year),
-      
       # Calculate gross income excluding OASI benefits
-      inc_ex_ss = wages + txbl_int + div + txbl_ira_dist + txbl_pens_dist + 
-                  txbl_kg + txbl_alimony + sole_prop + sch_e + farm + ui - nols + 
-                  other,
+      inc_ex_ss = wages + 
+        txbl_int + 
+        div + 
+        txbl_ira_dist + 
+        txbl_pens_dist + 
+        txbl_kg + 
+        alimony * (divorce_year < agi.alimony_repeal_year) + 
+        sole_prop + 
+        sch_e + 
+        farm +
+        ui - 
+        nols + 
+        other_inc,
       
-      # Calculate above-the-line deductions
-      alimony_ded = alimony_paid * (divorce_year < agi.alimony_repeal_year),
-      above_ded = ...
+      # Calculate above-the-line deductions, excluding student loan interest deduction 
+      above_ded_ex_sl = ed_exp + 
+        hsa_contr + 
+        liab_seca_er + 
+        keogh_contr + 
+        se_health + 
+        early_penalty + 
+        alimony_exp * (divorce_year < agi.alimony_repeal_year) + 
+        trad_contr_ira +
+        pmin(tuition_ded, agi.tuition_ded_limit) + 
+        pmin(dpad, agi.dpad_limit), 
+      
+      # Calculate MAGI for taxable Social Security benefits calculation
+      magi_ss = inc_ex_ss - above_ded_ex_sl
       
     ) %>% 
     
     # Calculate taxable social security benefits 
-    bind_cols(calc_taxable_ss(.)) %>% 
+    bind_cols(calc_ss(.)) %>% 
+    
     mutate(
       
-      # Calculate gross income and AGI
-      gross_inc = inc_ex_ss + taxable_ss, 
+      # Calculate student loan interest deduction
+      magi_sl     = inc_ex_ss + txbl_ss - above_ded_ex_sl,
+      sl_po_share = pmin(1, pmax(0, magi_sl - agi.sl_po_thresh) / agi.sl_po_range),
+      sl_int_ded  = pmin(sl_int_exp, agi.sl_limit) * (1 - sl_po_share),
+      
+      # Put all the pieces together
+      gross_inc = inc_ex_ss + txbl_ss,
+      above_ded = above_ded_ex_sl + sl_int_ded,
       agi       = gross_inc - above_ded
       
-    ) %>%
+    ) %>% 
     
     # Keep variables to return
-    select(txbl_ss, txbl_alimony, gross_inc, alimony_ded, above_ded, agi) %>% 
+    select(txbl_ss, sl_int_ded, gross_inc, above_ded, agi) %>% 
     return()
 }
+
 
