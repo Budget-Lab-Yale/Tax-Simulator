65d398a8e5137c6a35689d01a5fb49dc8cc6f6c6
ricco-budget-lab <john.ricco@yale.edu>
2025-01-23 15:09:54 -0500

    Add baby bonus phaseout

--- src/calc/functions/credits/ctc.R
+++ src/calc/functions/credits/ctc.R
@@ -51,40 +51,42 @@ calc_ctc = function(tax_unit, fill_missings = F) {
     'filer',                 # (int) whether tax unit files a tax return
     
     # Tax law attributes
-    'ctc.young_age_limit',  # (int) maximum age to qualify as "young" child
-    'ctc.old_age_limit',    # (int) maximum age to qualify as "old" child
-    'ctc.need_ssn',         # (int) whether SSN is required to qualify for CTC
-    'ctc.young_level',      # (int) whether to express value for young children in levels (in which case the calculator uses value_young) or in terms of difference from old (in which case the calculator adds bonus_young to value_old) 
-    'ctc.value_young1',     # (int) maximum credit value per "young" child corresponding to phaseout threshold 1 
-    'ctc.value_young2',     # (int) maximum credit value per "young" child corresponding to phaseout threshold 2
-    'ctc.bonus_young1',     # (int) maximum additional value per "young" child corresponding to phaseout threshold 1 
-    'ctc.bonus_young2',     # (int) maximum additional value per "young" child corresponding to phaseout threshold 2
-    'ctc.value_old1',       # (int) maximum credit value per "old" child corresponding to phaseout threshold 1
-    'ctc.value_old2',       # (int) maximum credit value per "old" child corresponding to phaseout threshold 2
-    'ctc.value_other',      # (int) maximum (nonrefundable) credit value per nonqualifying dependent (assumed to phase out with po_thresh1)
-    'ctc.po_thresh1',       # (int) AGI threshold above which value 1 (and nonqualifying dependent credit) phases out
-    'ctc.po_thresh2',       # (int) AGI threshold above which value 2 phases out
-    'ctc.po_type',          # (int) whether phaseout type is a rate (0 means range)
-    'ctc.po_rate1',         # (dbl) phaseout rate for value 1
-    'ctc.po_rate2',         # (dbl) phaseout rate for value 2 
-    'ctc.po_range1',        # (dbl) phaseout range for value 2
-    'ctc.po_range2',        # (dbl) phaseout range for value 1
-    'ctc.po_range_other',   # (dbl) non-child dependent credit phaseout range for married returns
-    'ctc.po_discrete',      # (int) whether phaseout is discretized, as in current-law form
-    'ctc.po_discrete_step', # (int) rounding step for discretized phaseout
-    'ctc.min_refund_level', # (int) whether to express minimum refundable CTC in dollar terms (in which case the calculator uses min_refund) or share of maximum value (in which case it uses min_refund_share)
-    'ctc.min_refund_young', # (int) minimum refundable CTC per young qualifying child
-    'ctc.min_refund_old',   # (int) minimum refundable CTC per old qualifying child
-    'ctc.min_refund_share', # (int) minimum refundable CTC as a share of total maximum credit
-    'ctc.max_refund_young', # (int) maximum refundable CTC per young qualifying child
-    'ctc.max_refund_old',   # (int) maximum refundable CTC per old qualifying child
-    'ctc.ei_prior_yr',      # (int) whether prior year earned income qualifies for phase-in
-    'ctc.pi_thresh',        # (int) earned income threshold above which CTC phases in
-    'ctc.pi_type',          # (int) whether phase-in type is a rate (0 means range)
-    'ctc.pi_rate',          # (dbl) phase-in rate
-    'ctc.pi_range',         # (dbl) phase-in range for total CTC (excluding nonqualifying dependent credit) 
-    'ctc.baby_bonus',       # (dbl) amount new parents are given in first month of a child's life
-    'ctc.baby_bonus_pi_rate'# (dbl) rate at which baby bonus phases in
+    'ctc.young_age_limit',     # (int) maximum age to qualify as "young" child
+    'ctc.old_age_limit',       # (int) maximum age to qualify as "old" child
+    'ctc.need_ssn',            # (int) whether SSN is required to qualify for CTC
+    'ctc.young_level',         # (int) whether to express value for young children in levels (in which case the calculator uses value_young) or in terms of difference from old (in which case the calculator adds bonus_young to value_old) 
+    'ctc.value_young1',        # (int) maximum credit value per "young" child corresponding to phaseout threshold 1 
+    'ctc.value_young2',        # (int) maximum credit value per "young" child corresponding to phaseout threshold 2
+    'ctc.bonus_young1',        # (int) maximum additional value per "young" child corresponding to phaseout threshold 1 
+    'ctc.bonus_young2',        # (int) maximum additional value per "young" child corresponding to phaseout threshold 2
+    'ctc.value_old1',          # (int) maximum credit value per "old" child corresponding to phaseout threshold 1
+    'ctc.value_old2',          # (int) maximum credit value per "old" child corresponding to phaseout threshold 2
+    'ctc.value_other',         # (int) maximum (nonrefundable) credit value per nonqualifying dependent (assumed to phase out with po_thresh1)
+    'ctc.po_thresh1',          # (int) AGI threshold above which value 1 (and nonqualifying dependent credit) phases out
+    'ctc.po_thresh2',          # (int) AGI threshold above which value 2 phases out
+    'ctc.po_type',             # (int) whether phaseout type is a rate (0 means range)
+    'ctc.po_rate1',            # (dbl) phaseout rate for value 1
+    'ctc.po_rate2',            # (dbl) phaseout rate for value 2 
+    'ctc.po_range1',           # (dbl) phaseout range for value 2
+    'ctc.po_range2',           # (dbl) phaseout range for value 1
+    'ctc.po_range_other',      # (dbl) non-child dependent credit phaseout range for married returns
+    'ctc.po_discrete',         # (int) whether phaseout is discretized, as in current-law form
+    'ctc.po_discrete_step',    # (int) rounding step for discretized phaseout
+    'ctc.min_refund_level',    # (int) whether to express minimum refundable CTC in dollar terms (in which case the calculator uses min_refund) or share of maximum value (in which case it uses min_refund_share)
+    'ctc.min_refund_young',    # (int) minimum refundable CTC per young qualifying child
+    'ctc.min_refund_old',      # (int) minimum refundable CTC per old qualifying child
+    'ctc.min_refund_share',    # (int) minimum refundable CTC as a share of total maximum credit
+    'ctc.max_refund_young',    # (int) maximum refundable CTC per young qualifying child
+    'ctc.max_refund_old',      # (int) maximum refundable CTC per old qualifying child
+    'ctc.ei_prior_yr',         # (int) whether prior year earned income qualifies for phase-in
+    'ctc.pi_thresh',           # (int) earned income threshold above which CTC phases in
+    'ctc.pi_type',             # (int) whether phase-in type is a rate (0 means range)
+    'ctc.pi_rate',             # (dbl) phase-in rate
+    'ctc.pi_range',            # (dbl) phase-in range for total CTC (excluding nonqualifying dependent credit) 
+    'ctc.baby_bonus',          # (dbl) refundable credit for newborn child
+    'ctc.baby_bonus_pi_rate',  # (dbl) phase-in rate for baby bonus
+    'ctc.baby_bonus_po_rate',  # (dbl) phase-out rate for baby bonus
+    'ctc.baby_bonus_po_thresh' # (dbl) phase-out threshold for baby bonus (AGI)
   )
   
   tax_unit %>% 
@@ -190,8 +192,12 @@ calc_ctc = function(tax_unit, fill_missings = F) {
       ctc_ref = pmin(min_refund + (pmax(0, qual_ei - ctc.pi_thresh) * pi_rate), 
                      pmin(remaining_ctc, max_refund_young + max_refund_old)),
       
+      # Calculate refundable baby bonus and add to refundable CTC
+      baby_bonus = pmin(max_value_baby, pmax(0, qual_ei) * ctc.baby_bonus_pi_rate),
+      baby_bonus = pmax(0, baby_bonus - pmax(0, agi - ctc.baby_bonus_po_thresh) * ctc.baby_bonus_po_rate),
+      
       # Phase in baby bonus
-      ctc_ref = ctc_ref + pmin(max_value_baby, pmax(0, qual_ei) * ctc.baby_bonus_pi_rate),
+      ctc_ref = ctc_ref + baby_bonus,
       
       # For $0-earning nonfilers, switch filing status if policy offers a refund
       become_filer_ctc = as.integer(filer == 0 & qual_ei == 0 & ctc_ref > 0)
