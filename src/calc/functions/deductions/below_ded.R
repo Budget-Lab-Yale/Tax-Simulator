07b4929f919d79526cc3ec5c3cbe28b7134d6196
kendall-budget-lab <joshua.m.kendall@yale.edu>
2025-05-29 12:03:50 -0400

    Senior bonus deduction. Changes config files, changes below calculations, removes from std and item ded

--- src/calc/functions/deductions/below_ded.R
+++ src/calc/functions/deductions/below_ded.R
@@ -33,20 +33,24 @@ calc_below_ded = function(tax_unit, fill_missings = F) {
     'tips_lh2',  # (int) whether tips2 is earned in a leisure and hospitality business
     'ot',        # (dbl) FLSA-eligible overtime income included in wages
     'agi',       # (dbl) Adjusted Gross Income
+    'age1',      # (int) age of primary filer
+    'age2',      # (int) age of secondary filer
 
     # Tax law attributes
-    'below.tip_ded_lh',         # (int) whether tips deduction is limited to leisure and hospitality workers only
-    'below.tip_ded_limit',      # (int) maximum deductible OT, non-joint returns
-    'below.tip_ded_po_thresh',  # (int) AGI threshold for OT deduction phaseout, non-joint returns
-    'below.tip_ded_po_type',    # (int) whether OT phaseout type is a rate (0 means range)
-    'below.tip_ded_po_rate',    # (int) phaseout rate for OT deduction
-    'below.tip_ded_po_range',   # (int) phaseout range for OT deduction
-    'below.ot_ded_half',        # (int) whether OT deduction applies only to the "half" of "time and a half"
-    'below.ot_ded_limit',       # (int) maximum deductible OT, non-joint returns
-    'below.ot_ded_po_thresh',   # (int) AGI threshold for OT deduction phaseout, non-joint returns
-    'below.ot_ded_po_type',     # (int) whether OT phaseout type is a rate (0 means range)
-    'below.ot_ded_po_rate',     # (int) phaseout rate for OT deduction
-    'below.ot_ded_po_range'     # (int) phaseout range for OT deduction
+    'below.tip_ded_lh',          # (int) whether tips deduction is limited to leisure and hospitality workers only
+    'below.tip_ded_limit',       # (int) maximum deductible OT, non-joint returns
+    'below.tip_ded_po_thresh',   # (int) AGI threshold for OT deduction phaseout, non-joint returns
+    'below.tip_ded_po_type',     # (int) whether OT phaseout type is a rate (0 means range)
+    'below.tip_ded_po_rate',     # (int) phaseout rate for OT deduction
+    'below.tip_ded_po_range',    # (int) phaseout range for OT deduction
+    'below.ot_ded_half',         # (int) whether OT deduction applies only to the "half" of "time and a half"
+    'below.ot_ded_limit',        # (int) maximum deductible OT, non-joint returns
+    'below.ot_ded_po_thresh',    # (int) AGI threshold for OT deduction phaseout, non-joint returns
+    'below.ot_ded_po_type',      # (int) whether OT phaseout type is a rate (0 means range)
+    'below.ot_ded_po_rate',      # (int) phaseout rate for OT deduction
+    'below.ot_ded_po_range',     # (int) phaseout range for OT deduction
+    'below.senior_ded_val',      # (int) value of senior bonus deduction
+    'below.senior_ded_po_thresh' # (int) AGI threshold for phaseout of senior bonus deduction
   )
   
   tax_unit %>% 
@@ -84,31 +88,15 @@ calc_below_ded = function(tax_unit, fill_missings = F) {
       # Limit base on AGI phaseout
       po_rate = if_else(below.ot_ded_po_type == 1, below.ot_ded_po_rate, ot_ded / below.ot_ded_po_range),
       ot_ded  = pmax(0, ot_ded - pmax(0, agi - below.ot_ded_po_thresh) * po_rate),
-      
-      
 
-      # TODO for josh -- move senior deduction here. 
-      # 
-      # the legislative wording is a bit strange. it says that for nonitemizers, 
-      # the 4K senior deduction is part of the standard deduction, but for 
-      # itemizers it's a separate below-the-line deduction. this means what we 
-      # have now, where it's included in itemized deductions for itemizers, 
-      # isn't technically correct, since the total amount itemized deductions are  
-      # an input into other tax calc functions like the AMT. 
-      # 
-      # this is tricky to implement because the choice between standard and 
-      # itemized deductions is not decided at this point in the execution. that 
-      # happens below in the function that determines txbl_inc. 
-      # 
-      # I think the easiest way to do this is to:
-      # 1) remove the existing code for the senior deduction and move its params
-      #    from std_ded.yaml to below_ded.yaml
-      # 2) do the calculation here and return a variable senior_ded
-      # 3) in calc_txbl_inc(), after itemizing status has been determined, 
-      #    add the amount of senior_below_ded to std_ded for nonitemizers and leave
-      #    it be for itemizers
       
-      senior_ded = 0, # placeholder 
+      #--------------------
+      # Senior deduction
+      #--------------------
+      age_senior1  = age1 >= 65,
+      age_senior2  = !is.na(age2) & (age2 >= 65),
+      senior_ded   = (age_senior1 + age_senior2) * below.senior_ded_val,
+      senior_ded   = pmax(0, senior_ded - .04 * pmax(0, agi - below.senior_ded_po_thresh))
       
     ) %>% 
     
